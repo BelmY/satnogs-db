75c6dad33ad3fb078b06c572b6709634d5980c0f
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2018-11-09 22:11:59 +0200

    Introduce SatNOGS Django control script
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

---
+++ bin/djangoctl.sh
@@ -0,0 +1,106 @@
+#!/bin/sh -e
+#
+# SatNOGS Django control script
+#
+# Copyright (C) 2018 Libre Space Foundation <https://libre.space/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+MANAGE_CMD="django-admin"
+
+usage() {
+	cat <<EOF
+Usage: $(basename "$0") [OPTIONS]... [COMMAND]...
+SatNOGS Django control script.
+
+COMMANDS:
+  prepare               Collect static files, compress templates and
+                         apply migrations
+  initialize            Load initial fixtures
+  run                   Run WSGI HTTP server
+  develop [SOURCE_DIR]  Run application in development mode, optionally
+                         installing SOURCE_DIR in editable mode
+
+OPTIONS:
+  --help                Print usage
+EOF
+	exit 1
+}
+
+prepare() {
+	"$MANAGE_CMD" collectstatic --noinput --clear
+	"$MANAGE_CMD" compress --force
+	"$MANAGE_CMD" migrate --noinput
+}
+
+start() {
+	exec "$MANAGE_CMD" runserver 0.0.0.0:8000
+}
+
+develop() {
+	if [ -d "$1" ]; then
+		pip install \
+		    --no-cache-dir \
+		    --no-deps \
+		    -r "${1}/requirements-dev.txt"
+		pip install \
+		    --no-cache-dir \
+		    --no-deps \
+		    --ignore-installed \
+		    -e "${1}"
+	fi
+	prepare
+	start
+}
+
+initialize() {
+	"$MANAGE_CMD" initialize
+}
+
+parse_args() {
+	while [ $# -gt 0 ]; do
+		arg="$1"
+		case $arg in
+			prepare|run|initialize)
+				command="$arg"
+				break
+				;;
+			develop)
+				shift
+				subargs="$1"
+				command="$arg"
+				break
+				;;
+			*)
+				usage
+				exit 1
+				;;
+		esac
+		shift
+	done
+}
+
+main() {
+	parse_args "$@"
+	if [ -z "$command" ]; then
+		usage
+		exit 1
+	fi
+	if [ -x "manage.py" ]; then
+		MANAGE_CMD="./manage.py"
+	fi
+	"$command" "$subargs"
+}
+
+main "$@"
