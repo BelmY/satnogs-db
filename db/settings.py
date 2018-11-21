96fd370157eefd62192d49a098fec9ba77f0583c
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2018-11-21 13:49:10 +0200

    Clean up settings and 'env-dist'
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- db/settings.py
+++ db/settings.py
@@ -5,8 +5,8 @@ from unipath import Path
 
 ROOT = Path(__file__).parent
 
-ENVIRONMENT = config('ENVIRONMENT', default='production')
-DEBUG = config('DEBUG', default=False, cast=bool)
+ENVIRONMENT = config('ENVIRONMENT', default='dev')
+DEBUG = config('DEBUG', default=True, cast=bool)
 AUTH0 = config('AUTH0', default=False, cast=bool)
 
 # Apps
@@ -70,11 +70,13 @@ SERVER_EMAIL = DEFAULT_FROM_EMAIL
 # Cache
 CACHES = {
     'default': {
-        'BACKEND': config('CACHE_BACKEND', default='redis_cache.RedisCache'),
-        'LOCATION': config('CACHE_LOCATION', default='unix://var/run/redis/redis.sock'),
+        'BACKEND': config('CACHE_BACKEND',
+                          default='django.core.cache.backends.locmem.LocMemCache'),
+        'LOCATION': config('CACHE_LOCATION', default='unique-location'),
         'OPTIONS': {
+            'MAX_ENTRIES': 5000,
             'CLIENT_CLASS': config('CACHE_CLIENT_CLASS',
-                                   default='django_redis.client.DefaultClient'),
+                                   default=''),
         },
         'KEY_PREFIX': 'db-{0}'.format(ENVIRONMENT),
     }
