ccc18b6c29c6c9cf23e5c2840025de0d2b1d3064
Corey Shields <cshields@gmail.com>
2018-12-12 13:09:27 -0500

    fix for sentry 804899643
    Signed-off-by: Corey Shields <cshields@gmail.com>

--- db/api/views.py
+++ db/api/views.py
@@ -41,7 +41,9 @@ class TelemetryView(viewsets.ModelViewSet, mixins.CreateModelMixin):
 
         norad_cat_id = request.data.get('noradID')
 
-        if not Satellite.objects.get(norad_cat_id=norad_cat_id).exists():
+        try:
+            sat = Satellite.objects.get(norad_cat_id=norad_cat_id) # noqa
+        except Satellite.DoesNotExist:
             try:
                 update_satellite(norad_cat_id, update_name=True, update_tle=True)
             except LookupError:
