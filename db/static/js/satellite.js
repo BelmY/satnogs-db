a8f5e53647df1a12a1203122ae46742bc74f3649
Pierros Papadeas <pierros@papadeas.gr>
2019-01-07 20:15:25 +0200

    Add UI for transmitter UUID

--- db/static/js/satellite.js
+++ db/static/js/satellite.js
@@ -1,3 +1,27 @@
+function copyToClipboard(text, el) {
+    var copyTest = document.queryCommandSupported('copy');
+    var elOriginalText = el.attr('data-original-title');
+
+    if (copyTest === true) {
+        var copyTextArea = document.createElement('textarea');
+        copyTextArea.value = text;
+        document.body.appendChild(copyTextArea);
+        copyTextArea.select();
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'Copied!' : 'Whoops, not copied!';
+            el.attr('data-original-title', msg).tooltip('show');
+        } catch (err) {
+            window.alert('Oops, unable to copy');
+        }
+        document.body.removeChild(copyTextArea);
+        el.attr('data-original-title', elOriginalText);
+    } else {
+        // Fallback if browser doesn't support .execCommand('copy')
+        window.prompt('Copy to clipboard: Ctrl+C or Command+C, Enter', text);
+    }
+}
+
 function suggestion_type(selection) {
     switch(selection){
     case 'Transmitter':
@@ -100,5 +124,12 @@ $(document).ready(function() {
     $('.frequency').each(function() {
         var to_format = $(this).html();
         $(this).html(format_freq(to_format));
+    });
+
+    // Copy UUIDs
+    $('.js-copy').click(function() {
+        var text = $(this).attr('data-copy');
+        var el = $(this);
+        copyToClipboard(text, el);
     });
 });
