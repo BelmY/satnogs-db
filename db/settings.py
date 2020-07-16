504392d1e75ff6aa8207c582a4f5c2bf80e2482a
Alfredos-Panagiotis Damkalis <fredy@fredy.gr>
2020-07-16 06:11:09 +0300

    Allow access to API endpoints for logged in users
    API endpoints that are behind authentication check, are now
    accessible by logged in users.
    
    Signed-off-by: Alfredos-Panagiotis Damkalis <fredy@fredy.gr>

--- db/settings.py
+++ db/settings.py
@@ -262,7 +262,10 @@ REDIS_CONNECT_RETRY = True
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':
     ('rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly', ),
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication', ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ],
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend', )
 }
 
